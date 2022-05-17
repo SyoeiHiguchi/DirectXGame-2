764d1f884aeb1ca678dd6f93f6c6606da9ca8b7b
SyoeiHiguchi <k021g1228@g.neec.ac.jp>
2022-05-17 13:35:54 +0900

    02_02_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -4,6 +4,65 @@
 #include "AxisIndicator.h"
 #include "PrimitiveDrawer.h"
 
+constexpr auto PI = 3.1415926;
+
+float ConvertToRadians(float Degrees) { return Degrees * (PI / 180.0f); }
+void ScaleMatrix(WorldTransform& wtf)
+{
+	Matrix4 matScale;//スケーリング行列を宣言
+	matScale.m[0][0] = wtf.scale_.x;//スケーリング倍率を行列に設定
+	matScale.m[1][1] = wtf.scale_.y;
+	matScale.m[2][2] = wtf.scale_.z;
+	matScale.m[3][3] = 1;
+	wtf.matWorld_ = MathUtility::Matrix4Identity();//単位行列を代入
+	wtf.matWorld_ *= matScale;
+	//wtf.TransferMatrix();//行列の転送
+}
+void TranslationMatrix(WorldTransform& wtf)
+{
+	Matrix4 matTrans = MathUtility::Matrix4Identity();//平行移動行列を設定
+	matTrans.m[3][0] = wtf.translation_.x;//移動量を行列に代入
+	matTrans.m[3][1] = wtf.translation_.y;
+	matTrans.m[3][2] = wtf.translation_.z;
+	wtf.matWorld_ *= matTrans;
+	wtf.TransferMatrix();//行列の転送
+}
+void RotationXMatrix(WorldTransform& wtf)
+{
+	Matrix4 matRotX = MathUtility::Matrix4Identity();;//x軸回転行列を宣言
+	matRotX.m[1][1] = cosf(wtf.rotation_.x);//x軸回転行列の各要素を設定する
+	matRotX.m[1][2] = sinf(wtf.rotation_.x);
+	matRotX.m[2][1] = -sinf(wtf.rotation_.x);
+	matRotX.m[2][2] = cosf(wtf.rotation_.x);
+	wtf.matWorld_ *= matRotX;
+}
+void RotationYMatrix(WorldTransform& wtf)
+{
+	Matrix4 matRotY = MathUtility::Matrix4Identity();//y軸回転行列を宣言
+	matRotY.m[0][0] = cosf(wtf.rotation_.y);//y軸回転行列の各要素を設定する
+	matRotY.m[2][0] = sinf(wtf.rotation_.y);
+	matRotY.m[0][2] = -sinf(wtf.rotation_.y);
+	matRotY.m[2][2] = cosf(wtf.rotation_.y);
+	wtf.matWorld_ *= matRotY;
+	wtf.TransferMatrix();//行列の転送
+}
+void RotationZMatrix(WorldTransform& wtf)
+{
+	Matrix4 matRotZ = MathUtility::Matrix4Identity();;//z軸回転行列を宣言
+	matRotZ.m[0][0] = cosf(wtf.rotation_.z);//z軸回転行列の各要素を設定する
+	matRotZ.m[0][1] = sinf(wtf.rotation_.z);
+	matRotZ.m[1][0] = -sinf(wtf.rotation_.z);
+	matRotZ.m[1][1] = cosf(wtf.rotation_.z);
+	wtf.matWorld_ *= matRotZ;
+	wtf.TransferMatrix();//行列の転送
+}
+void RotationMatrix(WorldTransform& wtf)
+{
+	RotationZMatrix(wtf);
+	RotationXMatrix(wtf);
+	RotationYMatrix(wtf);
+}
+
 GameScene::GameScene() {}
 
 GameScene::~GameScene() {
@@ -25,6 +84,15 @@ void GameScene::Initialize() {
 	AxisIndicator::GetInstance()->SetVisible(true);//軸方向表示の表示を有効化する
 	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());//軸方向が参照するビュープロダクションを指定する
 	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());//ライン描画するビュープロジェクションを指定する
+	
+	worldtransform_.scale_ = { 5.0f,5.0f,5.0f };//x,y,z方向のスケーリング設定
+	ScaleMatrix(worldtransform_);
+	float rad = ConvertToRadians(45.0f);
+	worldtransform_.rotation_ = { rad,rad,0.0f };//x,y,z軸周りの回転角を設定
+	RotationMatrix(worldtransform_);
+	worldtransform_.translation_ = { 10.0f,10.0f,10.0f };//x,y,z軸周りの平行移動を設定する
+	TranslationMatrix(worldtransform_);
+
 }
 
 void GameScene::Update() {
